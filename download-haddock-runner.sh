f08bf0946d26ab714b6aacda67951f3424672091
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2023-09-26 11:56:11 +0200

    add download-haddock-runner

---
+++ download-haddock-runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download the latest release of the `haddock-runner`
+export VERSION=1.6.3
+export URL=https://github.com/haddocking/haddock-runner/releases/download/v${VERSION}/haddock-runner_${VERSION}_linux_386.tar.gz
+
+# Download and extract the binary
+mkdir -p bin/
+cd bin/
+wget -qO- ${URL} | tar xvz
+rm LICENSE README.md
+chmod +x haddock-runner
+cd -
