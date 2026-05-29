31f45c637fa287b4200c3a4d257d2586b26c129e
Shantanu Khatri <88526089+Comp-era@users.noreply.github.com>
2026-05-29 18:18:47 +0200

    setup and scenarios added

---
+++ Docking_benchmarks/Protein_Glycan/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+# Download the protein-glycans dataset
+git clone --depth 1 https://github.com/haddocking/protein-glycans.git || true
+ 
+# Keep only pdb_files
+rm -rf protein-glycans/{.git,analysis,cfg_files,data,LICENSE,README.md,example_pic.png}
+ 
+# Create a `input-files.txt` file
+ls `pwd`/protein-glycans/pdb_files/**/*.{pdb,tbl} | grep -v "ana_scripts\|matched\|cg" | sort > prot-glycan-input.txt
