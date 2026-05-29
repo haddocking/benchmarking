30f9102416d4d49bbb9f699b6a3c48d248d8b72d
Shantanu Khatri <88526089+Comp-era@users.noreply.github.com>
2026-05-29 17:06:38 +0200

    removed protein-ligand-shape to update and move to a docking_benchmark

--- protein-ligand-shape/setup.sh
+++
@@ -1,7 +0,0 @@
-# Download benchmark and make it haddock3 haddock-runner ready
-python3 haddock2_to_haddock3.py
-python3 canonicalize_references.py
-# Copy a subset of the input list for testing
-cat input_list.txt | grep "/ppar" > input_list_ppar.txt
-# Download the Analysis script
-curl https://raw.githubusercontent.com/haddocking/haddock-tools/refs/heads/master/AnalyseBenchmarkResults.py > AnalyseBenchmarkResults.py 
