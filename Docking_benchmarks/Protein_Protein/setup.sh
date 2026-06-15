e6adfedad010f429e64c9d73bbb70f60f64f21d7
Shantanu Khatri <88526089+Comp-era@users.noreply.github.com>
2026-06-15 15:41:19 +0300

    add bm5-input.txt generation to Protein_Protein setup.sh, update README

--- Docking_benchmarks/Protein_Protein/setup.sh
+++ Docking_benchmarks/Protein_Protein/setup.sh
@@ -2,3 +2,6 @@
 
 # Download protein-protein BM5 benchmark dataset
 git clone https://github.com/haddocking/BM5-clean.git || true
+
+# Create input list with paths to all PDB, restraint, and topology files
+ls `pwd`/BM5-clean/HADDOCK-ready/**/*.{pdb,tbl,top,param} | grep -v "ana_scripts\|matched\|cg" | sort > bm5-input.txt
