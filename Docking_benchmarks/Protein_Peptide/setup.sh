0ed8c1902110641deda944277796215573f1054d
Shantanu Khatri <88526089+Comp-era@users.noreply.github.com>
2026-06-29 16:16:49 +0200

    Replace mdref with emref in Protein_Protein scenarios and remove ensemble rename step from Protein_Peptide setup

--- Docking_benchmarks/Protein_Peptide/setup.sh
+++ Docking_benchmarks/Protein_Peptide/setup.sh
@@ -3,10 +3,6 @@
 #Download and extract protein-peptide dataset
 git clone https://github.com/haddocking/protein-peptide protein-peptide-dataset
 
-#rename ligand ensemble files from *_l_u_ensemble.pdb to *_l_u.pdb so they match the input-list pattern
-find protein-peptide-dataset -name "*_l_u_ensemble.pdb" \
-  -exec bash -c 'mv "$0" "${0/_l_u_ensemble/_l_u}"' {} \;
-
 ls $(pwd)/protein-peptide-dataset/**/*.{pdb,tbl} \
 | grep -v "ana_scripts\|matched\|cg" \
 | grep -E "_r_u\.pdb$|_l_u\.pdb$|_b_ambig\.tbl$|_target\.pdb$" \
