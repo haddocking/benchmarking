fbc3ea96b17b8158aec8778a6ff62ae88c288c99
Rodrigo V. Honorato <rvhonorato@protonmail.com>
2026-07-17 09:48:48 +0200

    use commits from haddock3 repo as version

--- scripts/03_haddock3.sh
+++ scripts/03_haddock3.sh
@@ -3,8 +3,16 @@
 # HADDOCK3 + workflow deps into the venv from 01_python_env.sh.
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/_common.sh"
 
-echo "[+] Installing HADDOCK3 $HADDOCK3_VERSION"
-uv pip install --python "$VENV_PATH/bin/python" "haddock3==$HADDOCK3_VERSION"
+# Check if the version is from pypi or a commit
+if [[ "$HADDOCK3_VERSION" =~ ^[0-9a-fA-F]{7,40}$ ]]; then
+  echo "[+] Installing HADDOCK3 from commit $HADDOCK3_VERSION"
+  CMD="haddock3 @ git+https://github.com/haddocking/haddock3.git@$HADDOCK3_VERSION"
+else
+  echo "[+] Installing HADDOCK3 $HADDOCK3_VERSION from pypi"
+  CMD="haddock3==$HADDOCK3_VERSION"
+fi
+
+uv pip install --python "$VENV_PATH/bin/python" "$CMD"
 [ -x "$VENV_PATH/bin/haddock3" ] || {
   echo "[!!] HADDOCK3 install failed"
   exit 1
